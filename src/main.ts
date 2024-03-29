80c715b8af7e624e4dfc008533aeac0c929042fc
Konrad Palm <konrad.palm@alumni.fh-aachen.de>
2024-03-29 22:50:23 +0100

    added vue-router to project

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HelloWorld }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+createApp(App)
+    .use(router)
+    .mount('#app')
